1fdf8b3faaadaeac2197ba35d7b64aa9d6df5693
bviale <baptiste.viale@outlook.com>
2019-05-13 19:15:48 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# image-tracking
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/image-tracking)
---
+++ index.js
@@ -0,0 +1,33 @@
+require('https://trackingjs.com/bower/tracking.js/build/tracking-min.js')
+
+const img = new Image(300, 300);
+img.addEventListener('load', () => {
+  console.log('loaded')
+});
+img.src = 'https://i.ibb.co/VjdY0t8/Untitled.png';
+img.crossOrigin = "anonymous";
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+ctx.drawImage(img, 0, 0);
+
+var MyTracker = function() {
+  MyTracker.prototype.track = function(pixels, width, height) {
+    // Your code here
+    console.log('tracking')
+    this.emit('track', {
+      // Your results here
+    });
+  }
+}
+tracking.inherits(MyTracker, tracking.Tracker);
+
+
+var myTracker = new MyTracker();
+
+myTracker.on('track', function(event) {
+  // Results are inside the event
+});
+
+tracking.track(canvas, myTracker);
